525a010fa6077bc81d0b34cb6fc112d53ecefca1
Jan Kabela <h.kabela@gmail.com>
2024-01-19 15:38:49 +0100

    Fixed rspec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,9 +7,8 @@ RSpec.describe Book, type: :model do
 	  name: 'New book',
 	  author: 'John Doe',
 	  number_of_pages: 231,
-	  isbn: '9780321601919',
-	  publisher: 'Publisher',
-	  release_date: DateTime.now) 
+	  image_path: '/image', 
+	  url: 'url') 
   }
 
   it "is valid with valid attributes" do
@@ -28,21 +27,6 @@ RSpec.describe Book, type: :model do
   
   it "is not valid without a number of pages" do
     subject.number_of_pages = nil
-    expect(subject).to_not be_valid
-  end
-  
-  it "is not valid without a isbn" do
-    subject.isbn = nil
-    expect(subject).to_not be_valid
-  end
-   
-  it "is not valid without a publisher" do
-    subject.publisher = nil
-    expect(subject).to_not be_valid
-  end
-  
-  it "is not valid without a release date" do
-    subject.release_date = nil
     expect(subject).to_not be_valid
   end
   
